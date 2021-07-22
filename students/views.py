4d36d3cbceb65fcf40aa238892bd37c42dbe404d
Michael Kofi Tamakloe <michaeltamekloe18.mt@gmail.com>
2021-07-22 09:04:04 +0000

    Done with jod detials

--- students/views.py
+++ students/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'index.html', {'heading': 'Home'})
+
+
+def jobListing(request):
+    return render(request, 'jobListing.html', {'heading': "Job Listing"})
