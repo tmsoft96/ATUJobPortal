afeacc30e16043c404fbf5302daa31e4a29bb95a
Michael Kofi Tamakloe <michaeltamekloe18.mt@gmail.com>
2021-07-23 19:20:10 +0000

    split authention to another app module

--- ATUJobPortal/urls.py
+++ ATUJobPortal/urls.py
@@ -13,8 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from ATUJobPortal.authControllers.loginController import loginController
-from ATUJobPortal.authControllers.registerController import registerController
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('students.urls')),
-    path('register', registerController, name="register"),
-    path('login', loginController, name="login"),
+    path('', include('authentication.urls')),
 ]
---
+++ authentication/views.py
@@ -0,0 +1,8 @@
+from authentication.authControllers.loginController import loginController
+from authentication.authControllers.registerController import registerController
+
+def login(request):
+    return loginController(request)
+
+def register(request):
+    return registerController(request)
