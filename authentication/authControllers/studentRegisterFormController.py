9d5ce17502c6d6a8ff872e09d6ebccd5ec425a99
Michael Kofi Tamakloe <michaeltamekloe18.mt@gmail.com>
2021-07-24 10:27:06 +0000

    Display DOB from backend to frontend

--- authentication/authControllers/studentRegisterFormController.py
+++ authentication/authControllers/studentRegisterFormController.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+          'August', 'September', 'October', 'November', 'December']
+
 
 def studentRegisterFormController(request):
-    return render(request, 'studentRegisterForm.html', {"heading": "Create a Job Seeker Account | ATU Job Portal"})
+    dayList = []
+    yearList = []
+
+    for day in range(1, 32):
+        dayList.append(day)
+
+    for year in range(1900, 2015):
+        yearList.append(year)
+
+    yearList.reverse()
+    dobMap = {
+        "day": dayList,
+        "month": months,
+        "year": yearList,
+    }
+
+    return render(request,
+                  'studentRegisterForm.html',
+                  {"heading": "Create a Job Seeker Account | ATU Job Portal",
+                   "dob": dobMap, })
