8712915204cafba717cf54f9dee3ad89b1a89e7c
Michael Kofi Tamakloe <michaeltamekloe18.mt@gmail.com>
2021-07-26 07:07:26 +0000

    fix logout bug

--- ATUJobPortal/config/authentication.py
+++ ATUJobPortal/config/authentication.py
@@ -10,6 +10,9 @@ class Authentication:
                 if (request.session["authorize"]):
                     self.authMap["authorize"] = True
                     self.authMap["userType"] = request.session["userType"]
+                else:
+                    self.authMap["authorize"] = False
+                    self.authMap["userType"] = None
             except: 
                 self.authMap["authorize"] = False
                 self.authMap["userType"] = None
--- authentication/authControllers/logoutControler.py
+++ authentication/authControllers/logoutControler.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib import auth
 
+
 def logoutController(request):
     auth.logout(request)
     request.session["authorize"] = False
     request.session["userType"] = None
-    return render(request, 'login.html', {"heading": "login"})
+    return render(request, 'login.html',
+                  {"heading": "login"})
