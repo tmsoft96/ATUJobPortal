4f205a5d59b58768d86c3f5c613b1b0c47074e4d
Michael Kofi Tamakloe <michaeltamekloe18.mt@gmail.com>
2021-09-17 03:38:07 +0000

    done with firebase configuration

--- ATUJobPortal/config/firebase.py
+++ ATUJobPortal/config/firebase.py
@@ -1,4 +1,10 @@
-class FirebaseConfig:
-    config = {
-        "apiKey"
+class Firebase:
+    firebaseConfig = {
+        "apiKey": "AIzaSyD5-FOcfZTu_FeyMaZioPEz7MLpjSTYSfk",
+        "authDomain": "atujobportal-33aba.firebaseapp.com",
+        "projectId": "atujobportal-33aba",
+        "storageBucket": "atujobportal-33aba.appspot.com",
+        "messagingSenderId": "501965329940",
+        "appId": "1:501965329940:web:3f8bd58f8e0427a2c9b901",
+        "measurementId": "G-J9BRLQTB9R"
     }
