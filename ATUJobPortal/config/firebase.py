1a4960058f92ffada8b60a5ca05f769133603a1e
Michael Kofi Tamakloe <michaeltamekloe18.mt@gmail.com>
2021-09-17 03:45:41 +0000

    done with firebase configuration

--- ATUJobPortal/config/firebase.py
+++ ATUJobPortal/config/firebase.py
@@ -1,3 +1,4 @@
+import pyrebase
 class Firebase:
     firebaseConfig = {
         "apiKey": "AIzaSyD5-FOcfZTu_FeyMaZioPEz7MLpjSTYSfk",
@@ -6,5 +7,10 @@ class Firebase:
         "storageBucket": "atujobportal-33aba.appspot.com",
         "messagingSenderId": "501965329940",
         "appId": "1:501965329940:web:3f8bd58f8e0427a2c9b901",
-        "measurementId": "G-J9BRLQTB9R"
+        "measurementId": "G-J9BRLQTB9R",
+        "serviceAccount": "static/firebase/atujobportal-33aba-firebase-adminsdk-wcge9-31d51b269e.json",
     }
+
+    firebase = pyrebase.initialize_app(firebaseConfig)
+    authe = firebase.auth()
+    db = firebase.database()
