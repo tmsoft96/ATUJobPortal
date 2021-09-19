06c3dda553d046e024cd18ce37bedad430f716b4
Michael Kofi Tamakloe <michaeltamekloe18.mt@gmail.com>
2021-09-19 21:29:30 +0000

    done with post job but not completed yet

--- employers/controllers/employerPostJobController.py
+++ employers/controllers/employerPostJobController.py
@@ -1,3 +1,5 @@
+from django.http.response import HttpResponseRedirect
+from ATUJobPortal.config.firebase import Firebase
 from ATUJobPortal.config.dictionary import Dictionary
 from ATUJobPortal.config.authentication import Authentication
 from django.shortcuts import render
@@ -5,10 +7,36 @@ from django.shortcuts import render
 def employerPostJobController(request):
     auth = Authentication(request)
     dictionary = Dictionary()
+    firebase = Firebase()
+
     yearExperienceList = []
 
     for yearEx in range(1, 6):
         yearExperienceList.append(yearEx)
+
+    if request.method == "POST":
+        if request.POST.get("button") == "postJob":
+            job = {
+                "companyId": "",
+                "jobTitle": request.POST.get("jobTitle"),
+                "jobFunction": request.POST.get("jobFunction"),
+                "industry": request.POST.get("industry"),
+                "workType": request.POST.get("workType"),
+                "region": request.POST.get("region"),
+                "qualification": request.POST.get("qualification"),
+                "yearExperience": request.POST.get("yearExperience"),
+                "negotiable": request.POST.get("negotiable"),
+                "salary": request.POST.get("salary"),
+                "plusCommission": request.POST.get("plusCommission"),
+                "availableOpenings": request.POST.get("availableOpenings"),
+                "jobSummary": request.POST.get("jobSummary"),
+                "jobDescription": request.POST.get("jobDescription"),
+                "applyWith": "applyWithCoverLetter" if request.POST.get("applyWithCoverLetter") == "on" else "applyWithCoverLetterOrNot" ,
+                "viewBy": "viewByPortalAndEmail" if request.POST.get("viewByPortalAndEmail") == "on" else "viewByPortal" ,
+            }
+            firebase.db.child("Jobs").push(job)
+
+            return HttpResponseRedirect("/employer/dashboard?action=jobSuccess") 
 
     return render(request,
                   'employerPostJob.html',
