f5da1b81e2e1eae05357c11ffe143a1b2134da0b
Michael Kofi Tamakloe <michaeltamekloe18.mt@gmail.com>
2021-09-29 06:30:12 +0000

    configure mailgun

--- ATUJobPortal/settings.py
+++ ATUJobPortal/settings.py
@@ -133,3 +133,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# setting up mailgun
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'postmaster@sandboxa1162a11a2f24118b0ba01476e69b975.mailgun.org'
+EMAIL_HOST_PASSWORD = '66c333e34da848feb637ea13a4a2f410-dbdfb8ff-0c186c10'
+EMAIL_USE_TLS = True
